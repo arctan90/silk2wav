379e108206d044330ec8033f5aaaa3f9c4e16a2d
debuff <arctan90@163.com>
2024-01-24 10:20:53 +0800

    os and ioutil compatibility

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # silk2wav
+
 Convert wechat audio files (Skype silk format) to wav format
 
 # Build
-`go build -o ./bin/wechat2wav  ./cmd` Tested under MacOS X
+
+`go build -o ./bin/wechat2wav ./cmd`
+
+Tested under
+
+| Sys     | Checked |
+|---------|---------|
+| MacOS X | ✅       |
+| Ubuntu  | ✅       |
+| CentOS  | ✅       |
+| Windows | Todo    |
 
 # Usage
+
 ```bash
 chmod 755 ./bin/wechat2wav
 ./bin/wechat2wav inputfile outputfile
 ```
 
 # License
+
 forked from https://github.com/jdeng/silk2wav
 silk source code (/interface & /src) is from https://github.com/collects/silk and under the Skype license.
 silk.go is under the Apache license
--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"github.com/arctan90/silk2wav"
+	"io/fs"
+	"io/ioutil"
 	"log"
 	"os"
+	"runtime"
 )
 
 func main() {
@@ -12,7 +15,7 @@ func main() {
 		return
 	}
 
-	data, err := os.ReadFile(os.Args[1])
+	data, err := readFile(os.Args[1])
 	if err != nil {
 		log.Println(err)
 		return
@@ -24,8 +27,39 @@ func main() {
 		return
 	}
 
-	if err := os.WriteFile(os.Args[2], out, 0755); err != nil {
+	if err := writeFile(os.Args[2], out, 0755); err != nil {
 		log.Printf("main.go write file failed: %s\n", err.Error())
 	}
 	log.Printf("Converted %s to %s\n", os.Args[1], os.Args[2])
+}
+
+func readFile(name string) ([]byte, error) {
+	version := runtime.Version()
+	var data []byte
+	var err error
+	if version >= "go1.16" {
+		data, err = os.ReadFile(name)
+	} else {
+		data, err = ioutil.ReadFile(name)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func writeFile(name string, data []byte, perm fs.FileMode) error {
+	version := runtime.Version()
+	var err error
+	if version >= "go1.16" {
+		err = os.WriteFile(name, data, perm)
+	} else {
+		err = ioutil.WriteFile(name, data, perm)
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
